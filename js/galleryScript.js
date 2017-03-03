0e9a7071991948127389d61fd2d1c21780ce138e
Hardy173 <izvesten73@mail.ru>
2017-03-03 19:56:08 +0400

    site new
    Site

---
+++ js/galleryScript.js
@@ -0,0 +1,13 @@
+	$(document).ready(function() {
+		$("a.gallery1").fancybox( {	
+			openEffect : 'elastic',
+			openSpeed  : 500,
+			closeEffect : 'elastic',
+			closeSpeed  : 500,
+			arrows: true,
+			closeBtn: true,
+			closeClick: true
+		});
+	});
+
+	
---
+++ lab1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Первая лабораторная работа</title>
+</head>
+<body>
+
+<form method="post" action="result.php"> 
+<input id="arr1" name="arr1" type="text" placeholder="Массив 1"><br>
+<input id="arr2" name="arr2" type="text" placeholder="Массив 2"><br>
+<input type="submit" name="button" value="Выполнить все действия"><br>
+</form>
+<?php 
+	 
+
+?>
+	
+</body>
+</html>
